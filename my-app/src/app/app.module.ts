5f587ecde63af19c713ec971328228d0c25813c7
Beto <jrcampos82@gmail.com>
2023-05-11 16:44:51 -0400

    upd gaby

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeroComponent } from './components/hero/hero.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { GridComponent } from './components/grid/grid.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { GridComponent } from './components/grid/grid.component';
     HeroComponent,
     LoginComponent,
     GridComponent,
+    PageNotFoundComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
